8809df6e874fe34020f4f3586d65281e1549a0c0
233010 <233010@CARBON.CTI.P.LODZ.PL>
2020-01-16 09:48:35 +0100

    a

--- src/test/java/com/danix/code/smell/example001/CustomerTest.java
+++ src/test/java/com/danix/code/smell/example001/CustomerTest.java
@@ -1,6 +1,17 @@
 package com.danix.code.smell.example001;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * @author  danix
  */
-public class CustomerTest { }
+public class CustomerTest {
+
+    @Test
+    public void customerTest() {
+        assertThat(true, is(true));
+    }
+}
